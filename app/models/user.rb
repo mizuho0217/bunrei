85385b8fecf0c0e13918ce4f6cee796a8742389d
今津　瑞穂 <mizurinrinrin3@yahoo.co.jp>
2016-12-07 12:46:11 +0900

    今は、確認メールの再送信以外のdevise設定が終わった段階のところにいる。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
+    # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :examples
   has_many :iines
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205111939) do
+ActiveRecord::Schema.define(version: 20161206103728) do
 
   create_table "examples", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20161205111939) do
     t.datetime "updated_at",                                      null: false
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
